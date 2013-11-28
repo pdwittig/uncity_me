10bd7a79c4685c0978ad706d289682301f790723
Phil Wittig <pdwittig@gmail.com>
2013-11-28 18:37:50 +0200

    Updated park model to make all attributes accesible

--- app/models/park.rb
+++ app/models/park.rb
@@ -3,7 +3,7 @@ class Park < ActiveRecord::Base
 					length: { minimum: 5}
 	validates :latitude, presence: true
 	validates :longitude, presence: true
-	attr_accessible :city, :latitude, :longitude
+	attr_accessible :name, :city, :state, :country, :latitude, :longitude , :description
 	geocoded_by :city 
 	after_validation :geocode
 	has_many :activities
