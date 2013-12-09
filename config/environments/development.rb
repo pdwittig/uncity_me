b20d29d3b114ee7d4c5ca70b35f1006d2e5971b9
Phil Wittig <pdwittig@gmail.com>
2013-12-08 20:57:20 -0600

    ran some rake precompile stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ UncityMe::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Because rails says so - for assett pre complilation
+  config.assets.prefix = "/dev-assets"
 end
