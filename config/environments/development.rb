c10a5dacda81652a3ee92fe889d39e523320a55b
Dorota <dorota.kopczyk@gmail.com>
2014-01-29 00:34:47 -0600

    CSS on front page, but fucked up remaining pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ UncityMe::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.compile = true
 
   # Because rails says so - for assett pre complilation
   #config.assets.prefix = "/dev-assets"
